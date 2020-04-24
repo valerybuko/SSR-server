b584e411383b41b772f570913547d2625bf85f50
valerybuko <valpal314@gmail.com>
2020-04-24 11:38:35 +0300

    add entry pages

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import './index.css';
+import Card from './Card';
+
+export default () => (
+    <div className="App">
+        <header className="App-header">
+            <img src="/static/logo.png"
+                className="static-logo" alt="logo"
+            />
+        </header>
+        <div className="Grid">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
+)
